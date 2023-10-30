0e08f0039bd3fc679eb9e552d77cf443e5c33562
nahblue <ana.paula.alves11@gmail.com>
2023-10-30 16:55:04 -0300

    criação do component Tags

--- src/pages/Preview/index.jsx
+++ src/pages/Preview/index.jsx
@@ -1,10 +1,18 @@
-import { Header } from "../../components/Header"
 import { Container } from "./styles"
+
+import { Header } from "../../components/Header"
+import { Tags } from "../../components/Tags"
 
 export function Preview() {
   return(
     <Container>
       <Header />
+      <main>
+
+        <Tags title="Ficção Cientifica" />
+        <Tags title="Drama" />
+        <Tags title="Família" />
+      </main>
       
     </Container>
   )
